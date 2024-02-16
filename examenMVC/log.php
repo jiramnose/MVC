f491d107308d993cb63a82eb0072dcb3e5426703
Hiram <hiramglz44@gmail.com>
2024-02-15 18:03:11 -0600

    Update log.php

--- examenMVC/log.php
+++ examenMVC/log.php
@@ -1,7 +1,7 @@
 <?php
 function mensaje_log($mensaje) {
 $date = date ("Y-m-d H:i:5');
-$log message = "[{$date)] ($mensaje)\n";
+$log message = "[{$date)}] ({$mensaje})\n";
 $log_file = 'log.txt';
 file_put_contents($log_file, $log_message, FILE_APPEND | LOCK EX);
 ?>
